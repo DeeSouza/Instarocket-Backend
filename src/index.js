49a5cc440f935ad765aad5c42995975c5ea2ea19
Diego Souza <dee-souza@hotmail.com>
2019-06-11 10:49:43 -0300

    Feat: Create Model Post

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send();
-});
+mongoose.connect(
+  "mongodb+srv://root:SUr4TWYLUaa0JJDC@insta-c2ap1.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use(require("./routes"));
 
 app.listen(3333);
